6392f1bf7da68c0dff7c8e795590cc45cd351088
Philip Owusu <powusu6128@gmail.com>
2018-06-30 12:13:48 -0500

    auth.js

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -7,9 +7,13 @@ module.exports = function (app, passport) {
     app.get('/logout', authController.logout);
 
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/layouts/main.hbs',
-        failureRedirect: '/signup'
-    }));
+        // successRedirect: '/layouts/main.hbs',
+        // failureRedirect: '/signup'
+    }),
+        function(req, res) {
+            res.redirect("/layouts/main.hbs?user_id=" + req.user.id);
+        }
+    );
 
     app.post('/signin', passport.authenticate('local-signin', {
         successRedirect: '/dashboard',
